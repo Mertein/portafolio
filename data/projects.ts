71a00a3d681b3c851e1e0ba460eaf330e14c0c16
Martín Eduardo Aguayo <martineduardoaguayo@gmail.com>
2024-04-05 07:38:24 -0300

    02:

---
+++ data/projects.ts
@@ -0,0 +1,26 @@
+
+
+export const projects = [
+  {
+    id: '1',
+    name: 'Trello Clone',
+    description: 'A clone of the popular project management app Trello',
+    image: 'https://images.ctfassets.net/rz1oowkt5gyp/6REfiXE44STV6MKHXJbyRr/9df1e38e1c2f876f7491c88693bfdcad/TrelloGuideChapter1.jpg?w=1140&fm=webp',
+    link: 'https://trello.com/'
+  },
+  {
+    id: '2',
+    name: 'Next Auth V5 App',
+    description: 'Features: Two Factor (2FA), Forgot Password, OAuth (Google && GitHub), SSR, Form Actions and more..',
+    image: 'https://images.ctfassets.net/rz1oowkt5gyp/6REfiXE44STV6MKHXJbyRr/9df1e38e1c2f876f7491c88693bfdcad/TrelloGuideChapter1.jpg?w=1140&fm=webp',
+    link: 'https://next-auth.js.org/'
+  },
+  {
+    id: '3',
+    name: 'LMS (Learning Management System) platform',
+    description: 'Courses, Chapters, Payload, managment, User management, Admin Panel, and more..',
+    image: 'https://images.ctfassets.net/rz1oowkt5gyp/6REfiXE44STV6MKHXJbyRr/9df1e38e1c2f876f7491c88693bfdcad/TrelloGuideChapter1.jpg?w=1140&fm=webp',
+    link: 'https://www.google.com/'
+  }
+]
+
