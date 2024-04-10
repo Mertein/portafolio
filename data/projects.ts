394f0ab22e730357fa6161b4624b226adc913497
Martín Eduardo Aguayo <martineduardoaguayo@gmail.com>
2024-04-10 07:55:14 -0300

    03: carrousel

--- data/projects.ts
+++ data/projects.ts
@@ -1,26 +1,35 @@
-
-
 export const projects = [
   {
     id: '1',
     name: 'Trello Clone',
     description: 'A clone of the popular project management app Trello',
-    image: 'https://images.ctfassets.net/rz1oowkt5gyp/6REfiXE44STV6MKHXJbyRr/9df1e38e1c2f876f7491c88693bfdcad/TrelloGuideChapter1.jpg?w=1140&fm=webp',
+    images: [
+      'https://via.placeholder.com/400?text=Trello+Clone+Image+1',
+      'https://via.placeholder.com/400?text=Trello+Clone+Image+2',
+      'https://via.placeholder.com/400?text=Trello+Clone+Image+3',
+    ],
     link: 'https://trello.com/'
   },
   {
     id: '2',
     name: 'Next Auth V5 App',
     description: 'Features: Two Factor (2FA), Forgot Password, OAuth (Google && GitHub), SSR, Form Actions and more..',
-    image: 'https://images.ctfassets.net/rz1oowkt5gyp/6REfiXE44STV6MKHXJbyRr/9df1e38e1c2f876f7491c88693bfdcad/TrelloGuideChapter1.jpg?w=1140&fm=webp',
+    images: [
+      'https://via.placeholder.com/400?text=Next+Auth+V5+App+Image+1',
+      'https://via.placeholder.com/400?text=Next+Auth+V5+App+Image+2',
+      'https://via.placeholder.com/400?text=Next+Auth+V5+App+Image+3',
+    ],
     link: 'https://next-auth.js.org/'
   },
   {
     id: '3',
     name: 'LMS (Learning Management System) platform',
     description: 'Courses, Chapters, Payload, managment, User management, Admin Panel, and more..',
-    image: 'https://images.ctfassets.net/rz1oowkt5gyp/6REfiXE44STV6MKHXJbyRr/9df1e38e1c2f876f7491c88693bfdcad/TrelloGuideChapter1.jpg?w=1140&fm=webp',
+    images: [
+      'https://via.placeholder.com/400?text=LMS+Platform+Image+1',
+      'https://via.placeholder.com/400?text=LMS+Platform+Image+2',
+      'https://via.placeholder.com/400?text=LMS+Platform+Image+3',
+    ],
     link: 'https://www.google.com/'
   }
-]
-
+];
